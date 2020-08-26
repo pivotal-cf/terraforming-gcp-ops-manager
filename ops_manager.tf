7f56e0c791764ed475424c2482a0eb46356dc3ae
Ming Xiao <mxiao@pivotal.io>
2020-08-26 15:06:17 -0700

    Use terraform 0.11 syntax
    [#174452288] Use GCP images to boot system specs and push buttons in CI

--- ops_manager.tf
+++ ops_manager.tf
@@ -16,7 +16,7 @@ resource "google_compute_firewall" "ops-manager-external" {
 
 resource "google_compute_image" "ops-manager-image" {
   name           = "${var.env_name}-ops-manager-image"
-  count          = var.opsman_image_url == "" ? 0 : 1
+  count          = "${var.opsman_image_url == "" ? 0 : 1}"
   create_timeout = 20
 
   raw_disk {
