527b3d02ff313efa4b71b096bb0f28ed71d7d822
Kenneth Lakin <klakin@vmware.com>
2021-05-04 16:22:06 -0700

    Fix DNS entry generation
    The assigned_nat_ip that was referenced previously was always empty,
    causing errors on newer versions of Terraform. The corresponding nat_ip
    property did have an IP address in it.
    
    [#178007284] Fix AWS terraform in Ops Manager CI
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- dns.tf
+++ dns.tf
@@ -11,7 +11,7 @@ resource "google_dns_record_set" "ops-manager-dns" {
 
   managed_zone = google_dns_managed_zone.env_dns_zone.name
 
-  rrdatas = [google_compute_instance.ops-manager.network_interface[0].access_config[0].assigned_nat_ip]
+  rrdatas = [google_compute_instance.ops-manager.network_interface[0].access_config[0].nat_ip]
 }
 
 resource "google_dns_record_set" "optional-ops-manager-dns" {
@@ -22,7 +22,7 @@ resource "google_dns_record_set" "optional-ops-manager-dns" {
 
   managed_zone = google_dns_managed_zone.env_dns_zone.name
 
-  rrdatas = [google_compute_instance.optional-ops-manager[0].network_interface[0].access_config[0].assigned_nat_ip]
+  rrdatas = [google_compute_instance.optional-ops-manager[0].network_interface[0].access_config[0].nat_ip]
 }
 
 resource "google_dns_record_set" "wildcard-sys-dns" {
