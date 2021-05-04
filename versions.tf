5aa23de71ecdd28ff72051c5e70387f62a33e4bb
Kenneth Lakin <klakin@vmware.com>
2021-05-04 15:57:50 -0700

    Upgrade to Terraform 0.13
    We pinned back to 2.x google provider to avoid further changes to the
    template.
    
    [#178007284] Fix AWS terraform in Ops Manager CI
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- versions.tf
+++ versions.tf
@@ -1,4 +1,13 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = ">= 2.20.0, < 3.0.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
 }
