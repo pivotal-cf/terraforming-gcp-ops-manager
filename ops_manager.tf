ca16d3aded63dac36d093e8678ad945d864c637c
Long Nguyen <nguyenlo@vmware.com>
2022-07-21 10:28:00 -0400

    Updating to ECDSA as RSA isn't supported in jammy

--- ops_manager.tf
+++ ops_manager.tf
@@ -130,7 +130,7 @@ resource "google_storage_bucket" "director" {
 }
 
 resource "tls_private_key" "ops-manager" {
-  algorithm = "RSA"
-  rsa_bits  = "4096"
+  algorithm = "ECDSA"
+  ecdsa_curve  = "P256"
 }
 
